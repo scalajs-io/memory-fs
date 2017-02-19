95a4ae8909f9ad796fa239c279631e210277ce35
Lawrence Daniels <lawrence.daniels@gmail.com>
2017-02-19 14:02:17 -0800

    Upgraded to ScalaJs.io v0.3.0.5

--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ import sbt._
 import scala.language.postfixOps
 
 val apiVersion = "0.4.1"
-val scalaJsIOVersion = "0.3.0.4"
+val scalaJsIOVersion = "0.3.0.5"
 val scalaJsVersion = "2.12.1"
 
 homepage := Some(url("https://github.com/scalajs.io/memory-fs"))
--- src/main/scala/io/scalajs/npm/memoryfs/package.scala
+++ src/main/scala/io/scalajs/npm/memoryfs/package.scala
@@ -2,7 +2,7 @@ package io.scalajs.npm
 
 import io.scalajs.nodejs.buffer.Buffer
 import io.scalajs.nodejs.fs.{FileIOError, Stats}
-import io.scalajs.util.ScalaJsHelper._
+import io.scalajs.util.PromiseHelper._
 
 import scala.concurrent.Promise
 import scala.scalajs.js
@@ -21,35 +21,35 @@ package object memoryfs {
   final implicit class MemoryFSExtensions(val fs: MemoryFs) extends AnyVal {
 
     @inline
-    def existsFuture(path: String): Promise[Boolean] = futureCallbackE1[FileIOError, Boolean](fs.exists(path, _))
+    def existsFuture(path: String): Promise[Boolean] = promiseWithError1[FileIOError, Boolean](fs.exists(path, _))
 
     @inline
-    def mkdirpFuture(path: String): Promise[Unit] = futureCallbackE0[FileIOError](fs.mkdirp(path, _))
+    def mkdirpFuture(path: String): Promise[Unit] = promiseWithError0[FileIOError](fs.mkdirp(path, _))
 
     @inline
     def readdirFuture(path: String): Promise[Array[String]] =
-      futureCallbackE1[FileIOError, js.Array[String]](fs.readdir(path, _))
+      promiseWithError1[FileIOError, js.Array[String]](fs.readdir(path, _))
 
     @inline
     def readFileFuture(path: String, optArg: js.Any = null): Promise[Buffer] =
-      futureCallbackE1[FileIOError, Buffer](fs.readFile(path, optArg, _))
+      promiseWithError1[FileIOError, Buffer](fs.readFile(path, optArg, _))
 
     @inline
-    def rmdirFuture(path: String): Promise[Unit] = futureCallbackE0[FileIOError](fs.rmdir(path, _))
+    def rmdirFuture(path: String): Promise[Unit] = promiseWithError0[FileIOError](fs.rmdir(path, _))
 
     @inline
-    def statFuture(path: String): Promise[Stats] = futureCallbackE1[FileIOError, Stats](fs.stat(path, _))
+    def statFuture(path: String): Promise[Stats] = promiseWithError1[FileIOError, Stats](fs.stat(path, _))
 
     @inline
-    def unlinkFuture(path: String): Promise[Unit] = futureCallbackE0[FileIOError](fs.unlink(path, _))
+    def unlinkFuture(path: String): Promise[Unit] = promiseWithError0[FileIOError](fs.unlink(path, _))
 
     @inline
     def writeFileFuture(path: String, content: String, encoding: String = null): Promise[Unit] =
-      futureCallbackE0[FileIOError](fs.writeFile(path, content, encoding, _))
+      promiseWithError0[FileIOError](fs.writeFile(path, content, encoding, _))
 
     @inline
     def writeFileFuture(path: String, content: Buffer): Promise[Unit] =
-      futureCallbackE0[FileIOError](fs.writeFile(path, content, _))
+      promiseWithError0[FileIOError](fs.writeFile(path, content, _))
 
   }
 
